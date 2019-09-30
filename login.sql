f0f2d96cf63fde6f1c41c1ff072977c1d6a697cd
Piotr <piotr@kalina.cloud>
2019-09-30 12:01:51 +0200

    conf sqlplus default session via login.sql

---
+++ login.sql
@@ -0,0 +1,11 @@
+set timing off
+set tab off
+set time on
+set linesize 300
+set trims on
+set pages 55
+SET SERVEROUTPUT ON size unlimited
+WHENEVER SQLERROR CONTINUE
+set sqlprompt           "_USER'@'_CONNECT_IDENTIFIER > "
+define _editor=vi
+set echo on
